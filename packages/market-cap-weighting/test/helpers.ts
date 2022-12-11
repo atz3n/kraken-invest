e86b1af7f1ea29fc2fec78944511103cd8c3e78c
atz3n <attila.aldemir@atz3n.dev>
2022-12-11 15:00:40 +0100

    improve fail massage

--- packages/market-cap-weighting/test/helpers.ts
+++ packages/market-cap-weighting/test/helpers.ts
@@ -3,7 +3,7 @@ export function fail(message = "") {
     let failMessage = "";
     failMessage += "\n";
     failMessage += "FAIL FUNCTION TRIGGERED\n";
-    failMessage += "You see this because a fail function has been triggered";
+    failMessage += "The fail function has been triggered";
     failMessage += message ? " with message:" : "";
 
     expect(message).toEqual(failMessage);
