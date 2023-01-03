c32179c6232483a38b8ed2d173657fd748e0f128
atz3n <attila.aldemir@atz3n.dev>
2023-01-03 13:47:47 +0100

    add mapping check

--- packages/market-cap-weighting/src/main.ts
+++ packages/market-cap-weighting/src/main.ts
@@ -2,6 +2,7 @@ import { ConsoleTransport, FileTransport, initLogger, Kraken, logger } from "@at
 import { AssetMapper } from "./lib/AssetMapper";
 import { CoinGecko } from "./lib/CoinGecko";
 import { EnvVars } from "./lib/EnvVars";
+import { AssetMapping } from "./lib/IAssetMapper";
 import { initTasks } from "./schedule";
 import { IStateStore, State } from "./storage/state/IStateStore";
 import { createStateStore } from "./storage/state/stateStoreFactory";
@@ -26,7 +27,8 @@ async function main() {
     const assetMapper = new AssetMapper({
         uri: EnvVars.MAPPING_URI
     });
-    await assetMapper.updateMapping();
+    const mappings = await assetMapper.updateMapping();
+    checkMappings(mappings);
 
     logger.info("Init kraken...");
     const kraken = new Kraken({
@@ -67,6 +69,16 @@ async function initStateStore(stateStore: IStateStore): Promise<void> {
 
 function getVolume(state: State, asset: BaseAsset): number {
     return (state && (state.cumVolumes.find(volume => volume.symbol === asset.symbol))?.volume) || 0;
+}
+
+function checkMappings(mappings: AssetMapping[]): void {
+    for (let i = 0 ; i < EnvVars.BASE_ASSETS.length ; i++) {
+        const baseSymbol = EnvVars.BASE_ASSETS[i].symbol;
+        const mapping = mappings.find(_mapping => _mapping.krakenId === baseSymbol);
+        if (!mapping) {
+            throw new Error(`Mapping for base asset ${baseSymbol} not found. Add mapping first`);
+        }
+    }
 }
 
 main();
